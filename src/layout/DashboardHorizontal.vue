1d0982e356d0ae3f2984489a0a064047dce666b1
shancheas <shancheas@gmail.com>
2020-06-09 19:08:51 +0700

    create button logout

--- src/layout/DashboardHorizontal.vue
+++ src/layout/DashboardHorizontal.vue
@@ -1,93 +1,108 @@
 <template>
-    <div class="main-content bg-primary">
-        <!-- Navbar -->
-        <base-nav class="navbar-top navbar-horizontal navbar-dark"
-                  containerClasses="px-4 container"
-                  expand>
-            <router-link slot="brand" class="navbar-brand" to="/">
-                <img src="img/brand/white.png"/>
-            </router-link>
+  <div class="main-content bg-primary">
+    <!-- Navbar -->
+    <base-nav
+      class="navbar-top navbar-horizontal navbar-dark"
+      containerClasses="px-4 container"
+      expand
+    >
+      <router-link slot="brand" class="navbar-brand" to="/">
+        <img src="img/brand/white.png" />
+      </router-link>
 
-            <template v-slot="{closeMenu}">
-                <!-- Collapse header -->
-                <div class="navbar-collapse-header d-md-none">
-                    <div class="row">
-                        <div class="col-6 collapse-brand">
-                            <router-link to="/">
-                                <img src="img/brand/green.png">
-                            </router-link>
-                        </div>
-                        <div class="col-6 collapse-close">
-                            <button type="button"
-                                    @click="closeMenu"
-                                    class="navbar-toggler"
-                                    aria-label="Toggle sidenav">
-                                <span></span>
-                                <span></span>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <!-- Navbar items -->
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <router-link class="nav-link nav-link-icon" to="/">
-                            <i class="ni ni-planet"></i>
-                            <span class="nav-link-inner--text">Dashboard</span>
-                        </router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link nav-link-icon" to="/register">
-                            <i class="ni ni-circle-08"></i>
-                            <span class="nav-link-inner--text">Siswa</span>
-                        </router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link nav-link-icon" to="/profile">
-                            <i class="ni ni-single-02"></i>
-                            <span class="nav-link-inner--text">Guru</span>
-                        </router-link>
-                    </li>
-                </ul>
-            </template>
-        </base-nav>
-        <!-- Header -->
-        <div class="header py-7 py-lg-8">
-        </div>
-        <!-- Page content -->
-        <div class="container mt--8 pb-5">
-            <slide-y-up-transition mode="out-in" origin="center top">
-                <router-view></router-view>
-            </slide-y-up-transition>
-        </div>
-        <footer class="py-5">
-            <div class="container">
-                <div class="row align-items-center justify-content-xl-between">
-                    <div class="col-xl-6">
-                        <div class="copyright text-center text-xl-left text-muted">
-                            Copyright Tryout Online &copy; {{year}}
-                        </div>
-                    </div>
-                </div>
+      <template v-slot="{closeMenu}">
+        <!-- Collapse header -->
+        <div class="navbar-collapse-header d-md-none">
+          <div class="row">
+            <div class="col-6 collapse-brand">
+              <router-link to="/">
+                <img src="img/brand/green.png" />
+              </router-link>
             </div>
-        </footer>
+            <div class="col-6 collapse-close">
+              <button
+                type="button"
+                @click="closeMenu"
+                class="navbar-toggler"
+                aria-label="Toggle sidenav"
+              >
+                <span></span>
+                <span></span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <!-- Navbar items -->
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <router-link class="nav-link nav-link-icon" to="/">
+              <i class="ni ni-planet"></i>
+              <span class="nav-link-inner--text">Dashboard</span>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link nav-link-icon" to="/register">
+              <i class="ni ni-circle-08"></i>
+              <span class="nav-link-inner--text">Siswa</span>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link nav-link-icon" to="/profile">
+              <i class="ni ni-single-02"></i>
+              <span class="nav-link-inner--text">Guru</span>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link nav-link-icon" to="/login" @click.native="logOut">
+              <i class="fa fa-power-off"></i>
+              <span class="nav-link-inner--text">Keluar</span>
+            </router-link>
+          </li>
+        </ul>
+      </template>
+    </base-nav>
+    <!-- Header -->
+    <div class="header py-7 py-lg-8"></div>
+    <!-- Page content -->
+    <div class="container mt--8 pb-5">
+      <slide-y-up-transition mode="out-in" origin="center top">
+        <router-view></router-view>
+      </slide-y-up-transition>
     </div>
+    <footer class="py-5">
+      <div class="container">
+        <div class="row align-items-center justify-content-xl-between">
+          <div class="col-xl-6">
+            <div
+              class="copyright text-center text-xl-left text-muted"
+            >Copyright Tryout Online &copy; {{year}}</div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
 </template>
 <script>
-  import { SlideYUpTransition } from 'vue2-transitions'
+import { SlideYUpTransition } from "vue2-transitions";
 
-  export default {
-    name: 'auth-layout',
-    components: {
-      SlideYUpTransition
-    },
-    data() {
-      return {
-        year: new Date().getFullYear(),
-        showMenu: false
-      }
+export default {
+  name: "auth-layout",
+  components: {
+    SlideYUpTransition
+  },
+  data() {
+    return {
+      year: new Date().getFullYear(),
+      showMenu: false
+    };
+  },
+  methods: {
+    logOut() {
+      localStorage.clear();
+      this.$router.go()
     }
   }
+};
 </script>
 <style>
 </style>
